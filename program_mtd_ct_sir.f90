da1c18b54716a0ec697a5f68bca410a6b7c3125d
Abhinav Gupta <abhinav.rkg@gmail.com>
2020-03-11 16:57:06 +0530

    Add files via upload

--- program_mtd_ct_sir.f90
+++ program_mtd_ct_sir.f90
@@ -1,15 +1,22 @@
 ! PBMTD along phi1 and phi2 and TAMD along phi1,phi2,psi1,psi2
 PROGRAM reweighting
 implicit none
-real*8, allocatable :: hh(:), ss(:), grid_max(:), grid_min(:), grid_width(:), dum(:), ds2(:), dt(:), diff_s2(:)
-real*8, allocatable :: v(:,:), hill(:,:), ht(:,:), width(:,:),  grid(:,:), ct(:), pb_bias(:), num(:), den(:)
-real*8, allocatable :: prob(:,:,:), cv(:,:),vbias(:)
-real*8 :: kt,ktb,bias_fact,dum4,dum5,dum3,norm,t_cv,t_sys,deltaT,alpha_sys,alpha_cv,beta_sys,beta_cv,addition,dummy                 
-real*8 :: gridmin1,gridmin2, griddif1, griddif2, rweight, s1, s2, dum1,dum2, sys_temp                                                       
-real*8 :: dumm1, dumm2
-integer, allocatable :: nbin(:)
-integer :: i_mtd,mtd_steps,ns,ios,is,ig,i,j,i_md,md_steps,mtd_max,dum10
-integer :: w_cv, w_hill, k, t_min, t_max, index1, index2, i_s1, i_s2, np
+REAL*8, ALLOCATABLE :: hh(:), ss(:), grid_max(:), grid_min(:)
+REAL*8, ALLOCATABLE :: grid_width(:), dum(:), ds2(:), dt(:), diff_s2(:)
+REAL*8, ALLOCATABLE :: v(:,:), hill(:,:), ht(:,:), width(:,:)
+REAL*8, ALLOCATABLE ::  grid(:,:), ct(:), num(:), den(:), cv(:,:)
+
+REAL*8 :: bias_fact, norm, t_cv
+REAL*8 :: beta_cv, addition, t_sys                 
+REAL*8 :: gridmin1, gridmin2, griddif1, griddif2, rweight, s1, s2
+REAL*8 :: dum1, gamma_
+
+INTEGER, ALLOCATABLE :: nbin(:)
+
+INTEGER :: i_mtd, mtd_steps, ns, ios, is, ig, i, j, i_md
+INTEGER :: md_steps, mtd_max, dum2,  i_s1, i_s2, np
+INTEGER :: w_cv, w_hill, k, t_min, t_max, index1, index2
+
 REAL*8,PARAMETER :: pi=4.d0*atan(1.d0)
 REAL*8, PARAMETER :: kb=8.314472e-3                                                                  ! kJ/(mol*K)
 REAL*8, PARAMETER :: kj_to_kcal=1.d0/4.214d0
@@ -17,12 +24,10 @@ REAL*8, PARAMETER :: kj_to_kcal=1.d0/4.214d0
 open(1,file='input')
 
 read(1,*) ns
-read(1,*) t_cv, t_sys, deltaT                                   ! CVs_temp, system_temperature, bias_factor
+read(1,*) t_cv, t_sys, bias_fact                                   ! CVs_temp, system_temperature, bias_factor
 read(1,*) w_cv, w_hill                                ! frequency of printing cvmdck file, frequency of hill added 
 read(1,*) t_min, t_max
 
-np = w_hill/w_cv
-print *, 'np =', np
 open(2,file='HILLS_phi1')
 open(3,file='HILLS_phi3')
 open(7,file='HILLS_phi4')
@@ -60,8 +65,7 @@ allocate(den(ns))
 read(1,*) grid_max(1:ns)
 read(1,*) grid_min(1:ns)
 read(1,*) nbin(1:ns)
-read(1,*) mtd_steps
-!mtd_steps=10
+
 print *, 'bins used=', nbin(1:ns)
 
 grid_width(1:ns) = (grid_max(1:ns) - grid_min(1:ns))/float(nbin(1:ns)-1)
@@ -70,34 +74,27 @@ print *, 'grid widths =', grid_width(1:ns)
 
 
 do i=1,mtd_steps
-  read(2,*)   dum2, hill(1,i), width(1,i), ht(1,i), dum4
-  IF( hill(1,i) .gt. pi) hill(1,i) = hill(1,i) - 2.d0*pi
-  IF( hill(1,i) .lt. -pi) hill(1,i) = hill(1,i) + 2.d0*pi
+ read(2,*)   dum1, hill(1,i), width(1,i), ht(1,i), dum2
+          IF( hill(1,i) .gt. pi) hill(1,i) = hill(1,i) - 2.d0*pi
+         IF( hill(1,i) .lt. -pi) hill(1,i) = hill(1,i) + 2.d0*pi
 end do
 
 do i=1,mtd_steps
-  read(3,*)   dum3, hill(2,i), width(2,i), ht(2,i), dum5 
-  IF( hill(2,i) .gt. pi) hill(2,i) = hill(2,i) - 2.d0*pi
-  IF( hill(2,i) .lt. -pi) hill(2,i) = hill(2,i) + 2.d0*pi
+ read(3,*)   dum1, hill(2,i), width(2,i), ht(2,i), dum2 
+          IF( hill(2,i) .gt. pi) hill(2,i) = hill(2,i) - 2.d0*pi
+         IF( hill(2,i) .lt. -pi) hill(2,i) = hill(2,i) + 2.d0*pi
 end do
 
 do i=1,mtd_steps
-  read(7,*)   dum3, hill(3,i), width(3,i), ht(3,i), dum5
-  IF( hill(3,i) .gt. pi) hill(3,i) = hill(3,i) - 2.d0*pi
-  IF( hill(3,i) .lt. -pi) hill(3,i) = hill(3,i) + 2.d0*pi
+ read(7,*)   dum1, hill(3,i), width(3,i), ht(3,i), dum2
+          IF( hill(3,i) .gt. pi) hill(3,i) = hill(3,i) - 2.d0*pi
+         IF( hill(3,i) .lt. -pi) hill(3,i) = hill(3,i) + 2.d0*pi
 end do
 
-print *, 'mtd steps used =', mtd_steps
-!md_steps = mtd_steps*w_hill/w_cv +1
-!md_steps= mtd_steps*w_hill/w_cv
-
-print *, 'md steps used =', md_steps
-
-allocate(pb_bias(md_steps))
 allocate(cv(ns,md_steps))
 
 do k = 1, md_steps
-  read(4,*)   dum3, cv(1,k), dum3,dum3,dum3, cv(2,k),dum3,cv(3,k), dum3
+  read(4,*)   dum1,cv(1,k),dum1,dum1,dum1,cv(2,k),dum1,cv(3,k),dum1
               if (cv(1,k) .gt. pi)  cv(1,k) = cv(1,k) - 2.d0*pi
               if (cv(1,k) .lt. -pi) cv(1,k) = cv(1,k) + 2.d0*pi
               if (cv(2,k) .gt. pi)  cv(2,k) = cv(2,k) - 2.d0*pi
@@ -107,73 +104,62 @@ do k = 1, md_steps
 end do
 
 
-
-alpha_cv = (t_cv+deltaT)/deltaT
-alpha_sys=(t_sys+deltaT)/deltaT
 beta_cv =  1.d0/(kb*t_cv)
-beta_sys = 1.d0/(kb*t_sys)
+gamma_ = (bias_fact - 1.0)/bias_fact
 
-print *, 'alpha_cv = (t_cv+deltaT)/deltaT = ', alpha_cv
-print *, 'alpha_sys = (t_sys+deltaT)/deltaT = ', alpha_sys
+print *, 'gamma_ = ', gamma_
 print *, 'beta_cv = 1/(kb*t_cv) =  ', beta_cv
-print *, 'beta_sys = 1/(kb*t_sys) =  ', beta_sys
 
 allocate (grid(ns,nbin(1)))
 
 do i=1,nbin(1)
- grid(1,i) = grid_min(1) + float(i-1)*grid_width(1)
+grid(1,i) = grid_min(1) + float(i-1)*grid_width(1)
 end do
 
 do j=1,nbin(2)
- grid(2,j) = grid_min(2) + float(j-1)*grid_width(2)
+grid(2,j) = grid_min(2) + float(j-1)*grid_width(2)
 end do
 
 do j=1,nbin(3)
- grid(3,j) = grid_min(3) + float(j-1)*grid_width(3)
+grid(3,j) = grid_min(3) + float(j-1)*grid_width(3)
 end do
 
 allocate(v(ns,nbin(1)))
 
 print *, 'calculating ct factor.'
-i_md = w_cv*(np)
       v=0.d0
       DO i_mtd=1,mtd_steps
          ds2(1:ns)=width(1:ns,i_mtd)*width(1:ns,i_mtd)
          ss(1:ns)=hill(1:ns,i_mtd)
-         hh(1:ns)=ht(1:ns,i_mtd)/alpha_sys                  
+         
+         hh(1:ns)=ht(1:ns,i_mtd)                  
+         
          num=0.D0
          den=0.D0
-          DO is=1,ns
+         DO is=1,ns
 
-             DO ig=1,nbin(is)
+            DO ig=1,nbin(is)
               diff_s2(is)=grid(is,ig)-ss(is)
 
-                if (diff_s2(is) .gt. pi ) diff_s2(is) =diff_s2(is) - 2.d0*pi  
-                if (diff_s2(is) .lt.-pi ) diff_s2(is) =diff_s2(is) + 2.d0*pi 
+               if (diff_s2(is) .gt. pi ) diff_s2(is) =diff_s2(is) - 2.d0*pi  
+               if (diff_s2(is) .lt.-pi ) diff_s2(is) =diff_s2(is) + 2.d0*pi 
 
-               diff_s2(is)=diff_s2(is)*diff_s2(is)*0.5D0
-               v(is,ig) = v(is,ig) + hh(is)*dexp(-diff_s2(is)/ds2(is))               
+              diff_s2(is)=diff_s2(is)*diff_s2(is)*0.5D0
+              v(is,ig) = v(is,ig) + hh(is)*dexp(-diff_s2(is)/ds2(is))               
+              num(is) = num(is) + dexp(beta_cv*v(is,ig) - beta_cv*gamma_*v(is,ig))                 
+              den(is) = den(is) + dexp(beta_cv*v(is,ig))                                 
 
-    !          num(is) = num(is) + dexp(beta_cv*(alpha_sys-1.d0)*v(is,ig))                 
-    !          den(is) = den(is) + dexp(beta_cv*alpha_sys*v(is,ig))                                 
-
-              num(is) = num(is) + dexp(beta_cv*(alpha_cv-1.d0)*v(is,ig))                 
-              den(is) = den(is) + dexp(beta_cv*alpha_cv*v(is,ig))                                 
-             END DO
-          END DO
-
-      addition=0.d0
-            do is=1,ns
-            addition = addition + num(is)/den(is)! + 1.d0
-            end do       
-       
-      ct(i_mtd)= (1.d0/beta_cv)*dlog(addition)
-            
-            DO k = 1, np
-            write(20,'(3I10,F16.6)') k, i_md, i_mtd,  ct(i_mtd)
-            i_md = i_md + w_cv
             END DO
-      END DO
+         END DO
+
+         addition=0.d0
+         do is=1,ns
+          addition = addition + num(is)/den(is)! + 1.d0
+         end do       
+       
+         ct(i_mtd)= (1.d0/beta_cv)*dlog(addition)
+            write(20,'(I10,F16.6)')  i_mtd,  ct(i_mtd)
+         END DO
 close (20)
 print *, 'done calculating ct. => ct.dat'
 
@@ -194,14 +180,12 @@ deallocate(nbin)
 deallocate(grid_max)
 deallocate(grid_min)
 deallocate(grid_width)
-deallocate(pb_bias)
 deallocate(ct)
 deallocate(ds2)
 deallocate(diff_s2)
 deallocate(grid)
 deallocate(num)
 deallocate(den)
-!deallocate(prob)
 
 end program
 
